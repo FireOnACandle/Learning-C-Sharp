3d99fc6c2a3caf06af509338f30974f497be7e10
Experiencer <60224515+FireOnACandle@users.noreply.github.com>
2020-03-02 23:20:47 +0100

    Update Story.cs

--- Story.cs
+++ Story.cs
@@ -9,20 +9,20 @@ namespace Story
             Console.WriteLine("");
             Console.WriteLine("Choose your gender:");
             Console.WriteLine("Male or Female?");
-            Console.WriteLine("");
-            string PlayerGender = Console.ReadLine();    
+            Console.WriteLine("");  
         }
             public static void FemaleCharacters(string Kimberly, string Anna, string Laura)
             {
-                Console.WriteLine("");
-                Console.WriteLine("Choose one of these characters to play as:");
+             Console.WriteLine("");
+             Console.WriteLine("Choose one of these characters to play as:");
 
-                Console.WriteLine(Kimberly);
-                Console.WriteLine("");
-                Console.WriteLine(Anna);
-                Console.WriteLine("");
-                Console.WriteLine(Laura);
-                Console.WriteLine("");
+             Console.WriteLine("");
+             Console.WriteLine(Kimberly);
+             Console.WriteLine("");
+             Console.WriteLine(Anna);
+             Console.WriteLine("");
+             Console.WriteLine(Laura);
+             Console.WriteLine("");
 
             }
             public static void MaleCharacters(string Paul, string Mark, string Tom)
@@ -38,20 +38,24 @@ namespace Story
             Console.WriteLine("");
         }
 
-            static void Main(string[] args)
+        static void Main(string[] args)
+        {
+            ChoosingGender("Male", "Female");
+            string PlayerGender = Console.ReadLine();
+            string PlayerCharacterMale;
+            string PlayerCharacterFemale;
+
+            if (PlayerGender is "Male")
             {
-                ChoosingGender("Male", "Female");
-
-                if (PlayerGender is "Male")
-                {
                 MaleCharacters("Paul", "Mark", "Tom");
-                }
-                else if (PlayerGender is "Female")
-                {
-                    FemaleCharacters("Kimberly", "Anna", "Laura");
-                    string PlayerCharacterFemale = Console.ReadLine();
-                }
-
+                PlayerCharacterMale = Console.ReadLine();
+            }
+            else if (PlayerGender is "Female")
+            {
+                FemaleCharacters("Kimberly", "Anna", "Laura");
+                PlayerCharacterFemale = Console.ReadLine();
             }
         }
+        }
     }
+
