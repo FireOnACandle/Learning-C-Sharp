0f445ce9fbb105c6f1c0b24b6248bee3cf8d61b8
Experiencer <60224515+FireOnACandle@users.noreply.github.com>
2020-03-03 14:18:02 +0100

    Update Story.cs

--- Story.cs
+++ Story.cs
@@ -2,60 +2,171 @@ using System;
 
 namespace Story
 {
-        public class Program
-        {
-            public static void BREAK()
+    public class Program
+    {
+        public static string PlayerGender;
+        public static string PlayerCharacter;
+        public static string Decision1;
+        public static string Hunger;
+
+        public static void BREAK()
         {
             Console.WriteLine("-----------");
         }
-            public static void ChoosingGender(string Male, string Female)
+        public static void ChoosingCharacter(string Male = "Male", string Female = "Female")
         {
             Console.WriteLine("");
             Console.WriteLine("Choose your gender:");
             Console.WriteLine("Male or Female?");
-            Console.WriteLine("");  
-        }
-            public static void FemaleCharacters(string Kimberly, string Anna, string Laura)
+            Console.WriteLine("");
+            PlayerGender = Console.ReadLine();
+
+            if (PlayerGender is "Male")
             {
-             Console.WriteLine("");
-             Console.WriteLine("Choose one of these characters to play as:");
-
-             Console.WriteLine("");
-             Console.WriteLine(Kimberly);
-             Console.WriteLine("");
-             Console.WriteLine(Anna);
-             Console.WriteLine("");
-             Console.WriteLine(Laura);
-             Console.WriteLine("");
-
+                MaleCharacters();
+                BREAK();
+                PlayerCharacterMale();
+                BREAK();
             }
-            public static void ChoosePlayerCharacterFemale()
+            else if (PlayerGender is "Female")
+            {
+                FemaleCharacters();
+                BREAK();
+                PlayerCharacterFemale();
+                BREAK();
+            }
+            else
+            {
+                ChoosingCharacter();
+            }
+
+            Decision1 = Console.ReadLine();
+
+            if (Decision1 is "Yes")
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Alright, let the first day begin");
+            }
+            else if (Decision1 is "No")
+            {
+                ChoosingCharacterSITUATIONAL();
+            }
+            else
+            {
+                Decision1SITUATIONAL();
+            }
+        }
+        public static void ChoosingCharacterSITUATIONAL()
         {
-            string PlayerCharacterFemale = Console.ReadLine();
-            
-            switch (PlayerCharacterFemale)
+            Console.WriteLine("");
+            Console.WriteLine("Choose your gender:");
+            Console.WriteLine("Male or Female?");
+            Console.WriteLine("");
+            PlayerGender = Console.ReadLine();
+
+            if (PlayerGender is "Male")
+            {
+                MaleCharacters();
+                BREAK();
+                PlayerCharacterMale();
+                BREAK();
+            }
+            else if (PlayerGender is "Female")
+            {
+                FemaleCharacters();
+                BREAK();
+                PlayerCharacterFemale();
+                BREAK();
+            }
+            else
+            {
+                ChoosingCharacter();
+            }
+            Decision1 = Console.ReadLine();
+
+            if (Decision1 is "Yes")
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Alright, let the first day begin");
+            }
+            else if (Decision1 is "No")
+            {
+                ChoosingCharacterSITUATIONAL();
+            }
+            else
+            {
+                Decision1SITUATIONAL();
+            }
+        }
+        public static void Decision1SITUATIONAL()
+        {
+            Decision1 = Console.ReadLine();
+            if (Decision1 is "Yes")
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Alright, let the first day begin");
+            }
+            else if (Decision1 is "No")
+            {
+                ChoosingCharacterSITUATIONAL();
+            }
+            else
+            {
+                Decision1SITUATIONAL();
+            }
+        }
+
+        public static void FemaleCharacters(string Kimberly = "Kimberly", string Anna = "Anna", string Laura = "Laura")
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Choose one of these characters to play as:");
+
+            Console.WriteLine("");
+            Console.WriteLine(Kimberly);
+            Console.WriteLine("");
+            Console.WriteLine(Anna);
+            Console.WriteLine("");
+            Console.WriteLine(Laura);
+            Console.WriteLine("");
+        }
+        public static void PlayerCharacterFemale()
+        {
+            Console.WriteLine("If you want to go back, type: back");
+            PlayerCharacter = Console.ReadLine();
+
+            switch (PlayerCharacter)
             {
                 case "Kimberly":
                     Console.WriteLine("");
-                    Console.WriteLine("This is Kimberly. \n");
+                    Console.WriteLine("This is Kimberly.\nShe's a rather cheerful girl.\nHer likes consist of:\nSpending time with her friends\nSweet foods\nTheme Parks");
+                    Console.WriteLine("");
+                    Console.WriteLine("Would you like to play as Kimberly? (type Yes or No)");
                     break;
 
                 case "Anna":
                     Console.WriteLine("");
-                    Console.WriteLine("This is Anna. \n");
+                    Console.WriteLine("This is Anna.\nShe's a rather shy girl.\nHer likes consist of:\nSpending time with her friend Emily\nPizza\nListening to music");
+                    Console.WriteLine("");
+                    Console.WriteLine("Would you like to play as Anna? (type Yes or No)");
                     break;
 
                 case "Laura":
                     Console.WriteLine("");
-                    Console.WriteLine("This is Laura. \n");
+                    Console.WriteLine("This is Laura.\nShe's pretty adventurous.\nHer likes consist of:\nTrying new things\nEating exotic foods\nExiting activities");
+                    Console.WriteLine("");
+                    Console.WriteLine("Would you like to play as Laura? (type Yes or No)");
+                    break;
+
+                case "back":
+                    ChoosingCharacter();
+                    break;
+
+                default:
+                    PlayerCharacterFemale();
                     break;
             }
         }
-            public static void FemaleCharacterAttributes()
-        {
-            BREAK();
-        }
-            public static void MaleCharacters(string Paul, string Mark, string Tom)
+        public static void MaleCharacters(string Paul = "Paul", string Mark = "Mark", string Tom = "Tom")
         {
             Console.WriteLine("");
             Console.WriteLine("Choose one of these characters to play as:");
@@ -68,51 +179,50 @@ namespace Story
             Console.WriteLine(Tom);
             Console.WriteLine("");
         }
-            public static void ChoosePlayerCharacterMale()
-            {
-            string PlayerCharacterMale = Console.ReadLine();
+        public static void PlayerCharacterMale()
+        {
+            Console.WriteLine("If you want to go back, type: back");
+            PlayerCharacter = Console.ReadLine();
 
-            switch (PlayerCharacterMale)
+            switch (PlayerCharacter)
             {
                 case "Paul":
                     Console.WriteLine("");
-                    Console.WriteLine("This is Paul. \n");
+                    Console.WriteLine("This is Paul.\nPaul is a funny and pretty positive guy.\nHis likes consist of:\nPlaying his guitar\nSpending time with his friends\nJunk food");
+                    Console.WriteLine("");
+                    Console.WriteLine("Would you like to play as Paul? (type Yes or No)");
                     break;
 
                 case "Mark":
                     Console.WriteLine("");
-                    Console.WriteLine("This is Mark. \n");
-                    break;
-
-                case "Tim":
+                    Console.WriteLine("This is Mark.\nMark is a pretty serious person\nHis likes consist of:\nStudying\nListening to music\nSour foods");
                     Console.WriteLine("");
-                    Console.WriteLine("This is Tom. \n");
+                    Console.WriteLine("Would you like to play as Mark? (type Yes or No)");
+                    break;
+
+                case "Tom":
+                    Console.WriteLine("");
+                    Console.WriteLine("This is Tom.\nHe's a rather shy person\nHis likes consist of:\nPlaying video games\nSpending time with his friend Frederick\nSweet foods");
+                    Console.WriteLine("");
+                    Console.WriteLine("Would you like to play as Tom? (type Yes or No)");
+                    break;
+
+                case "back":
+                    ChoosingCharacter();
+                    break;
+
+                default:
+                    PlayerCharacterMale();
                     break;
             }
-            BREAK();
-                
-            }
 
-            public static void Main(string[] args)
-            {
-                 ChoosingGender("Male", "Female");
-                 string PlayerGender = Console.ReadLine();
+        }
 
-                if (PlayerGender is "Male")
-            {
-                MaleCharacters("Paul", "Mark", "Tom");
-                BREAK();
-                ChoosePlayerCharacterMale();
-            }
-                else if (PlayerGender is "Female")
-            {
-                FemaleCharacters("Kimberly", "Anna", "Laura");
-                BREAK();
-                ChoosePlayerCharacterFemale();
-                FemaleCharacterAttributes();
-            }
-
-            }
+        public static void Main(string[] args)
+        {
+            ChoosingCharacter();
         }
     }
+}
+
 
